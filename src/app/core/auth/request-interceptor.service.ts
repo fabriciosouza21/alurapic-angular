754aef29f3082644d541adcec03bfcfbcfad43fa
Fabricio Miranda <fabricio@itexto.com.br>
2022-10-14 09:48:16 -0300

    intercept

---
+++ src/app/core/auth/request-interceptor.service.ts
@@ -0,0 +1,27 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { TokenService } from '../token/token.service';
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class RequestInterceptorService implements HttpInterceptor {
+  constructor(
+    private tokenService: TokenService
+  ) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.tokenService.hasToken()) {
+      const token = this.tokenService.getToken();
+      req = req.clone({
+        setHeaders: {
+          'x-access-token': token || ''
+        }
+      });
+    }
+    return next.handle(req);
+  }
+
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptorService } from './auth/request-interceptor.service';
 
 
 
@@ -13,6 +15,13 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     RouterModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptorService,
+      multi: true
+    }
+  ]
 })
 export class CoreModule { }
